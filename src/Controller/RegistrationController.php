0d8d7c6301b8edaaf80e18d26500865a8232f080
Audrey Gambus <audrey.gambus@gmail.com>
2026-07-09 22:17:02 +0200

    feat:ajout de Mailtrap et de la fonctionnalité de réinitialisation de mot de passe, testée et fonctionnelle

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,11 +12,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\RoleRepository; // Récupération du rôle utilisateur en base
+
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, RoleRepository $roleRepository): Response
     {
         $user = new Utilisateur();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -28,11 +30,16 @@ class RegistrationController extends AbstractController
 
             // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            $user->setRoles(['ROLE_USER']); // Attribution du rôle Symfony natif
+            $user->setActif(true);
+
+            // Récupération du rôle "ROLE_USER" depuis l'entité Role
+            $roleUser = $roleRepository->findOneBy(['libelle' => 'ROLE_USER']);
+            // Lien avec l'utilisateur
+            $user->setRole($roleUser);
 
             $entityManager->persist($user);
             $entityManager->flush();
-
-            // do anything else you need here, like send an email
 
             return $security->login($user, LoginFormAuthenticator::class, 'main');
         }
