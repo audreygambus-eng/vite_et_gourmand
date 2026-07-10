39aef37fafa61da49d8f2389a7d1731aec056f1e
Audrey Gambus <audrey.gambus@gmail.com>
2026-07-10 22:03:44 +0200

    fix: ajout de la gestion d'erreur sur l'envoi des mails

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,16 +12,17 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address; // Affichage du nom de l'expéditeur
+use Symfony\Component\Mime\Address; // Affichage du nom de l'expéditeur, associé à son adresse mail
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\RoleRepository; // Récupération du rôle utilisateur en base
+use Psr\Log\LoggerInterface; // Traçage des erreurs d'envoi de mails
 
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, RoleRepository $roleRepository, MailerInterface $mailer): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, RoleRepository $roleRepository, MailerInterface $mailer, LoggerInterface $logger): Response
     {
         $user = new Utilisateur();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -44,16 +45,22 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $email = (new TemplatedEmail())
-            ->from(new Address('contact@vite-et-gourmand.com', 'Vite & Gourmand'))
-            ->to((string) $user->getEmail())
-            ->subject('Bienvenue chez Vite & Gourmand !')
-            ->htmlTemplate('emails/bienvenue.html.twig')
-            ->context([
-                'prenom' => $user->getPrenom(),
-            ]);
-            $mailer->send($email);
-
+            // Envoi du mail isolé pour ne pas bloquer l'inscription si Mailtrap échoue
+            try{
+                $email = (new TemplatedEmail())
+                    ->from(new Address('contact@vite-et-gourmand.com', 'Vite & Gourmand'))
+                    ->to((string) $user->getEmail())
+                    ->subject('Bienvenue chez Vite & Gourmand !')
+                    ->htmlTemplate('emails/bienvenue.html.twig')
+                    ->context([
+                        'prenom' => $user->getPrenom(),
+                    ]);
+                $mailer->send($email);
+            }
+            catch (\Exception $e) {
+                // Erreur tracée sans blocage d'inscription
+                $logger->error('Erreur envoi mail de bienvenue : ' . $e->getMessage());
+            }
             return $security->login($user, LoginFormAuthenticator::class, 'main');
         }
 
