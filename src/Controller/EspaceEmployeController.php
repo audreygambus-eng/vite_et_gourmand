ef1401feb569183ff0d44313ba7d6b807ea329c6
Audrey Gambus <audrey.gambus@gmail.com>
2026-07-14 16:24:50 +0200

    feat: ajout de la liste des menus dans l'espace employé avec leur statut actif ou inactif

--- src/Controller/EspaceEmployeController.php
+++ src/Controller/EspaceEmployeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,9 +12,17 @@ class EspaceEmployeController extends AbstractController
 {
     #[Route('/espace/employe', name: 'app_espace_employe')]
     #[IsGranted('ROLE_EMPLOYE')]
-
     public function index(): Response
     {
         return $this->render('espace_employe/index.html.twig');
+    }
+
+    #[Route('/espace/employe/menus', name: 'app_espace_employe_menus')]
+    #[IsGranted('ROLE_EMPLOYE')]
+    public function menus(MenuRepository $menuRepository): Response
+    {
+        return $this->render('espace_employe/menus.html.twig', [
+            'menus' => $menuRepository->findAll(),
+        ]);
     }
 }
