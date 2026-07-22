7ce86a2cc00e06fa2de1b0dab411e258cf0e656f
Audrey Gambus <audrey.gambus@gmail.com>
2026-07-22 12:11:31 +0200

    feat: intégration de Cloudinary pour un stockage persistant des images de menus (Flysystem + VichUploaderBundle)

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
