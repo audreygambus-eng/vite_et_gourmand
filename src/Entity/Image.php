b462ec0ae2d5d76a815c2e30e4883627af349c0e
Audrey Gambus <audrey.gambus@gmail.com>
2026-09-22 16:12:22 +0200

    chore: remplacement de l'import déprécié de VichUploader

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Vich\UploaderBundle\Mapping\Attribute as Vich;
 use Symfony\Component\HttpFoundation\File\File;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
